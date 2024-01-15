a402e9ea5096510de95f092e6a59b8cfca9f8fff
ativmob-willianderson <willianderson.sn@gmail.com>
2024-01-15 14:42:48 -0300

    added two_sum resolution

--- leetcode/questions_solution.php
+++ leetcode/questions_solution.php
@@ -1,4 +1,19 @@
 <?php
 
-echo "hello";
+function twoSum($nums, $target)
+{
+    for ($i = 0; $i < count($nums); $i++) {
+        for ($j = $i + 1; $j < count($nums); $j++) {
+            if (($nums[$i] + $nums[$j]) == $target) {
+                echo $nums[$i] + $nums[$j];
+                return [$i, $j];
+            }
+        }
+    }
+    throw new Exception("Error Processing Request", 1);
+    
+}
+
+twoSum([3, 2, 3], 6);
+
 ?>
